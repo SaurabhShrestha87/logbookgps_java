270c22beff8a2398783c0859a93c2520bad7176c
saurabh <shresthasaurabh86@gmail.com>
2024-03-28 12:40:32 +0530

    completed.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ wget https://download2.gluonhq.com/openjfx/17.0.10/openjfx-17.0.10_linux-x64_bin
 unzip openjfx-17.0.10_linux-x64_bin-sdk.zip
 
 # Set up the ANDROID_HOME environment variable
-echo 'export ANDROID_HOME=~/Android/sdk' >> ~/.bashrc
+echo 'export ANDROID_HOME=~/Android/Sdk' >> ~/.bashrc
 
 # Set up the ANDROID_EMULATOR_HOME environment variable
 echo 'export ANDROID_EMULATOR_HOME=$ANDROID_HOME/emulator' >> ~/.bashrc
@@ -21,4 +21,4 @@ echo 'export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOM
 # Inform the user to source the ~/.bashrc file manually
 source ~/.bashrc
 
-echo "You can close this terminal and open a new one to start using the Android SDK tools.
+echo "You can close this terminal and open a new one to start using the Android SDK tools."
